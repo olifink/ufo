54065e6e5afd91f73e26fc9c2f76822ec9f4e8a0
Oliver Fink <323250+olifink@users.noreply.github.com>
2023-05-29 17:49:32 +0200

    simple player movememnt

--- player.gd
+++ player.gd
@@ -1,8 +1,14 @@
 extends RigidBody2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	test()
+func _physics_process(delta):
+	var force = 1000
+	if Input.is_action_pressed("move_right"):
+		apply_force(Vector2(force, 0))
+	if Input.is_action_pressed("move_left"):
+		apply_force(Vector2(-force, 0))
+	if Input.is_action_pressed("move_down"):
+		apply_force(Vector2(0, force))
+	if Input.is_action_pressed("move_up"):
+		apply_force(Vector2(0, -force))
 
-func test():
-	print("...here is some more...")
+
