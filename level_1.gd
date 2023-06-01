65f7347bb25c3a04fb8cad5ee0f5a5a087a70ad8
Oliver Fink <323250+olifink@users.noreply.github.com>
2023-06-01 19:04:34 +0200

    background, collision detection, labels

--- level_1.gd
+++ level_1.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Hello World")
+	pass
 
 
+
+
+func _on_maze_body_exited(body):
+	print("body exited")
+	get_tree().reload_current_scene()
--- player.gd
+++ player.gd
@@ -1,7 +1,8 @@
 extends RigidBody2D
 
+var force = 1000
+
 func _physics_process(delta):
-	var force = 1000
 	if Input.is_action_pressed("move_right"):
 		apply_force(Vector2(force, 0))
 	if Input.is_action_pressed("move_left"):
